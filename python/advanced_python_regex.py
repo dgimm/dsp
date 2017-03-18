52070bffae76056269635b2d6f1c5a17d5f1949f
dgimm <091046@gmail.com>
2017-03-17 22:06:07 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,4 +1,4 @@
-#Q6
+#Q1
 import pandas as pd
 
 faculty = pd.read_csv('Desktop/metis/prework/dsp/python/faculty.csv')
@@ -15,4 +15,18 @@ faculty_degree = faculty_degree.stack()
 
 faculty_degree.value_counts()
 
-#Q7
+#Q2
+faculty['title'] = faculty['title'].replace('is', 'of', regex=True)
+faculty['title'] = faculty['title'].replace('Assoftant', 'Assistant', regex=True)
+
+faculty['title'].value_counts()
+
+#Q3
+faculty_email_list = list(faculty['email'])
+
+print(faculty_email_list)
+
+#4
+faculty_email = faculty['email'].str.extract('(@.+)')
+
+faculty_email.value_counts()
