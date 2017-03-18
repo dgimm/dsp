743dd2be446b7492eee81c020df2643337f01579
dgimm <091046@gmail.com>
2017-03-17 22:03:53 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,18 @@
-PLACE YOUR CODE HERE
+#Q6
+import pandas as pd
+
+faculty = pd.read_csv('Desktop/metis/prework/dsp/python/faculty.csv')
+
+faculty = faculty.rename(columns=lambda x: x.strip())
+
+faculty['degree'] = faculty['degree'].str.replace('.', '')
+faculty['degree'] = faculty['degree'].str.replace('.', '')
+faculty['degree'] = faculty['degree'].str.strip()
+
+faculty_degree = faculty['degree']
+faculty_degree = faculty_degree.str.split(' ').apply(pd.Series)     
+faculty_degree = faculty_degree.stack()
+
+faculty_degree.value_counts()
+
+#Q7
