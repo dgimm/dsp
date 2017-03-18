66dccac945770abfe8c2960e907514dff395898f
dgimm <091046@gmail.com>
2017-03-17 22:11:53 -0400

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,6 @@
-PLACE YOUR CODE HERE
+import csv
+
+with open('emails.csv', "w") as f:
+    writer = csv.writer(f, lineterminator='\n')
+    for val in faculty_email_list:
+        writer.writerow([val])
